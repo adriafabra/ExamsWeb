cf34945347a67865dec8e2bc549986319cb4f01e
solso10 <64212966+solso10@users.noreply.github.com>
2021-05-31 08:47:47 +0200

    Update Exams.java

--- src/java/udl/Exams.java
+++ src/java/udl/Exams.java
@@ -27,10 +27,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- *
- * @author adri
- */
+
 @Entity
 @Table(name = "EXAMS")
 @XmlRootElement
